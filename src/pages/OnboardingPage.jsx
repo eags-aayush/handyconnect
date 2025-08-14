e85a3779a1e5195c2f3073504d1b4f413daf25a0
Aayush Laddha <aayushladdha001@gmail.com>
2025-08-14 19:09:54 +0530

    testing apk 1

--- src/pages/OnboardingPage.jsx
+++ src/pages/OnboardingPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function OnboardingPage() {
 
+    const navigate = useNavigate();
     const loginHandler = () => {
-
+        navigate('/profile');
     }
 
     return (
