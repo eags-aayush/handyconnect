03960b5b0ee93062b06beda4aae8c6a883f347b3
Aayush Laddha <aayushladdha001@gmail.com>
2025-08-25 15:50:36 +0530

    .

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -69,9 +69,9 @@ const Profile = () => {
             Contact Us
           </span>
           <ul className="mt-3 bg-white shadow-sm rounded-xl overflow-hidden divide-y divide-gray-100">
-            <li className="py-4 px-4 font-medium text-gray-700">
+            <a className="py-4 px-4 font-medium text-gray-700" href="tel:+918420996300">
               +91 8420996300
-            </li>
+            </a>
             <li className="py-4 px-4 font-medium text-gray-700">
               contact@handyconnect.in
             </li>
